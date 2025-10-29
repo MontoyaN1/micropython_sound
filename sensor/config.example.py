c6327aabefe372ef716b01504b4f28036edfdb6a
MontoyaN1 <juanpablomontoyajpmv@gmail.com>
2025-10-29 10:26:20 -0500

    config_example

--- sensor/config.example.py
+++ sensor/config.example.py
@@ -2,6 +2,8 @@ WIFI_SSID = "tu_ssid_aqui"
 WIFI_PASSWORD = "tu_password_aqui"
 MQTT_BROKER = "broker.hivemq.com"
 MQTT_PORT = 1883
+
+# De ser necesario seguridad, para el caso no se hizo
 MQTT_USER = "usuario"
 MQTT_PASSWORD = "password"
 CLIENT_ID = "dispositivo_01"
