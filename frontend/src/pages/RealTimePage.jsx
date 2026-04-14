8f6435ee4eb383b75989244866f6fd642401801e
MontoyaN1 <juanpablomontoyajpmv@gmail.com>
2026-04-14 15:22:18 -0500

    fix: timestamp de UI en formato pm/am

--- frontend/src/pages/RealTimePage.jsx
+++ frontend/src/pages/RealTimePage.jsx
@@ -197,7 +197,7 @@ const RealTimePage = () => {
     const day = String(date.getDate()).padStart(2, "0");
     const month = String(date.getMonth() + 1).padStart(2, "0");
     const year = date.getFullYear();
-    const time = date.toLocaleTimeString("es-CO", { hour12: false });
+    const time = date.toLocaleTimeString("es-CO", { hour12: true });
     return `${day}/${month}/${year} ${time}`;
   };
 
